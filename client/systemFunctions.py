b9d0a2f335c3375e9930b88b9777a505977dc930
plokamar1 <p.dorgiak@gmail.com>
2017-04-13 14:42:28 +0300

    Trying to make the return json string correct

--- client/systemFunctions.py
+++ client/systemFunctions.py
@@ -1,5 +1,4 @@
 import psutil, json, string, time
-from bitmath import *
 import os
 
 def CPUStats():
@@ -9,18 +8,18 @@ def CPUStats():
     
     nmbCores = len(percentPerCpu)
 
-    retStr = "\"CPUStats\": [\n\t\"CurrentUsage\"{"
+    retStr = "\"CPUStats\": {\n\t\"CurrentUsage\":["
     for i in range(1,nmbCores+1):
         retStr += "\n\t\t\"Core_"+str(i)+"\" : \""+ str( percentPerCpu[i-1]/nmbCores ) +""
         if(i < nmbCores):
             retStr += ','
-    retStr += "\n\t},\n"
+    retStr += "\n\t],\n"
     avgTimes = len(loadAverage)
-    retStr +="\t\"loadAverage\": {\n"
+    retStr +="\t\"loadAverage\": [\n"
     retStr +="\t\t\"oneMinute\":\""+str(loadAverage[0])+"\",\n"
     retStr +="\t\t\"fiveMinutes\":\""+str(loadAverage[1])+"\",\n"
     retStr +="\t\t\"fifteenMinutes\":\""+str(loadAverage[2])+"\"\n"
-    retStr +="\t}\n]"
+    retStr +="\t]\n}"
     print(retStr)
 
 def DISKStats():
@@ -32,7 +31,6 @@ def DISKStats():
             diskUsage_gb = ('%.2f' % (diskUsage[1]/1073741824)) + " Gb"
 
             diskIO = psutil.disk_io_counters()
-            print(diskIO)
 
             eachPart_stats ={ "Name": partition[1], "usagePercent":diskUsage[3], "usageGB":diskUsage_gb, "read_count":diskIO[0], "write_count":diskIO[1] }
             partitions_stats.append(eachPart_stats)
