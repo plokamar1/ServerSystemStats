d4db4a8a3732b13ac837002d21ce407bb0b66bac
plokamar1 <p.dorgiak@gmail.com>
2017-04-19 10:43:30 +0300

    Creating a basic configuration file and reading from it

--- client/client.py
+++ client/client.py
@@ -1,12 +1,24 @@
 #libraries import
-import psutil, time, json
-
+import psutil, time, json, os
+from configparser import SafeConfigParser
 #scripts import
 from systemFunctions import *
+from networkFunctions import *
+
+
+config = SafeConfigParser()
+if os.path.isfile(os.getcwd()+'\client\config.ini'):
+    print(config.read(os.getcwd()+'\client\config.ini'))
+    print(config.sections())
+    get_data = config.getint('Settings','Get_Status_Every')
+    print(get_data)
 
 
 if __name__ == "__main__":
-    i = 1
-    while ( i>0 ):
-        print(CPUStats())
-        time.sleep(30)
+    if os.path.isfile(os.getcwd()+'\config.ini'):
+        print('Its here')
+    # i = 1
+    # while ( i>0 ):
+    #     print(CPUStats())
+    #     time.sleep(30)
+    i=0
