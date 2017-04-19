ab8c8d544ee3a1100c11b2c0021233cb09528ac7
plokamar1 <p.dorgiak@gmail.com>
2017-04-19 12:18:46 +0300

    Now checking if a library exists and if it is not, then gets installed

--- client/client.py
+++ client/client.py
@@ -1,24 +1,33 @@
 #libraries import
 import psutil, time, json, os
 from configparser import SafeConfigParser
+
 #scripts import
 from systemFunctions import *
 from networkFunctions import *
 
+#got this code from rominf in stack overflow
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
 
-config = SafeConfigParser()
-if os.path.isfile(os.getcwd()+'\client\config.ini'):
-    print(config.read(os.getcwd()+'\client\config.ini'))
-    print(config.sections())
-    get_data = config.getint('Settings','Get_Status_Every')
-    print(get_data)
-
+install_and_import('transliterate')
 
 if __name__ == "__main__":
-    if os.path.isfile(os.getcwd()+'\config.ini'):
-        print('Its here')
-    # i = 1
+    install_and_import('psutil')
+    install_and_import('daemon')
+    config = SafeConfigParser()
+    if os.path.isfile(os.getcwd()+'\client\config.ini'):
+        print(config.read(os.getcwd()+'\client\config.ini'))
+        print(config.sections())
+        sleep_time = config.getint('Settings','Get_Status_Every')
+
     # while ( i>0 ):
     #     print(CPUStats())
     #     time.sleep(30)
-    i=0
